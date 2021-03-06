72368213a95bccb0419c71de40070155fcce8550
AryaneLadeira <aryanemottals@gmail.com>
2021-03-05 22:01:41 -0300

    Adicionando get e set

--- Aula 1/PrimeiroCodigo/Aluno.java
+++ Aula 1/PrimeiroCodigo/Aluno.java
@@ -5,10 +5,31 @@ public class Aluno //Aluno se torna um tipo de dado
    String nome;
    double nota;
    
+   //Método Construtor
    public Aluno(String pNome, double pNota){
        nome = pNome;
        nota = pNota;
    }
+   
+   //Métodos de Acesso
+   public void setNota(double novaNota){
+       if (novaNota >= 0 && novaNota <= 10){
+            nota = novaNota;
+        }
+   }
+   
+   public double getNota(){
+        return nota;
+   }
+   
+   public void setNome(String novoNome){
+       nome = novoNome; 
+   }
+   
+   public String getNome(){
+       return nome.toUpperCase();
+   }
+   
    
    //Metodos
    public boolean estaAprovado(){
